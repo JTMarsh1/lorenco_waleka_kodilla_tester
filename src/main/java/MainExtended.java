14ebfa6e540ede57acbe5bf7ec82cd167958f4d9
Lorenco Waleka <lorenco.waleka96@gmail.com>
2022-10-01 11:45:39 +0200

    Zadania

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,19 @@
+
+public class MainExtended{
+    public static void main(String[] args) {
+        String myname= "Lorenzo";
+        int mojwiek = 26;
+        double obwodwklatce= 99.8;
+        boolean obwodklatce = false ;
+        String ulubionysamcohod = "BMW" ;
+        boolean umiemprogramowac = false;
+        System.out.println(myname);
+        System.out.println(mojwiek);
+        System.out.println(obwodwklatce);
+        System.out.println(obwodklatce);
+        System.out.println(ulubionysamcohod);
+        System.out.println(umiemprogramowac) ;
+    }
+
+
+}
