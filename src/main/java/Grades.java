5a2c4e2f37ec992bb0ad73bc5c4a91b0fb3852ad
Lorenco Waleka <lorenco.waleka96@gmail.com>
2022-11-08 17:55:07 +0100

    ess

--- src/main/java/Grades.java
+++ src/main/java/Grades.java
@@ -5,6 +5,7 @@ public class Grades {
         this.grades = new int[10];
         this.size = 0;
     }
+
     public void add(int value) {
         if (this.size == 10) {
             return;
@@ -12,9 +13,11 @@ public class Grades {
         this.grades[this.size] = value;
         this.size++;
     }
+
     public int lastGrade(){
     return this.grades[this.size -1];
     }
+
     public double averageGrades() {
     double sum =0;
     for (int i = 0; i < this.size; i++ ) {
